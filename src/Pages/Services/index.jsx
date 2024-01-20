879bc3ce2bd7d009085a7618cb799ece3f5c8e7b
QulamSamadli <qulam999@gmail.com>
2024-01-21 01:02:25 +0400

    add new codes to component

--- src/Pages/Services/index.jsx
+++ src/Pages/Services/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import HeaderBottom from '../../Components/HeaderBottom'
 
+
+const _url = "http://localhost:5381/solutions"
+
 const Services = () => {
   return (
     <div>
 <HeaderBottom title1="salam" title2="salam2" title3="salam1"/>
+
+
 
     </div>
   )
