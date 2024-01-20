46a87741b5a0545ea796110a9b9bd3a90014c7ed
QulamSamadli <qulam999@gmail.com>
2024-01-21 01:30:45 +0400

    new codes added

--- src/Pages/Services/index.jsx
+++ src/Pages/Services/index.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
-import HeaderBottom from '../../Components/HeaderBottom'
+import React, { useEffect, useState } from "react";
+import HeaderBottom from "../../Components/HeaderBottom";
+import axios from "axios";
 
-
-const _url = "http://localhost:5381/solutions"
+const _url = "http://localhost:5381/solutions";
 
 const Services = () => {
+  const [solution, setSolution] = useState([]);
+  useEffect(() => {
+    axios.get(_url).then(({ data }) => {
+      setSolution(data);
+    });
+  }, []);
+
   return (
-    <div>
-<HeaderBottom title1="salam" title2="salam2" title3="salam1"/>
+    <div className="container ">
+      <HeaderBottom title1="salam" title2="salam2" title3="salam1" />
 
-
-
+      <div className="flex flex-wrap justify-center">
+      {solution.map(({ id, img, title, description }) => {
+        return (
+          <div className="border-[#D4DAEA] w-[290px] p-4 flex flex-col gap-2 border-[1px] rounded-[10px]">
+            <img src={img} alt="" />
+            <h2 className="text-[18px] h-14 text-[#435072] font-bold">{title}</h2>
+            <hr />
+            
+            <p className="text-[13px]  text-[#606F84] font-normal">
+              {description}
+            </p>
+          </div>
+        );
+      })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
