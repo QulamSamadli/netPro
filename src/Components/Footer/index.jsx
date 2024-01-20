a3e39422803a16d2bc9ac6a8c943fd81dbd87bd6
QulamSamadli <qulam999@gmail.com>
2024-01-21 02:02:17 +0400

    changes footer components

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="bg-footerBg ">
-      <div className="container flex py-10 justify-between ">
+    <div className="  bg-footerBg ">
+      <div className=" container flex py-4 justify-between  ">
         <div className="flex">
-          <div className="left1 flex flex-col justify-between">
+          <div className=" flex flex-col justify-between">
             <div>
               <img src="./footerLogo.png" alt="" />
             </div>
@@ -34,9 +34,9 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-1">
           <h2 className=" text-2xl font-bold text-[white]">Saytın xəritəsi</h2>
-          <div className="text-[#ffffff8c] flex flex-col gap-5 ">
+          <div className="text-[#ffffff8c] flex flex-col gap-2 ">
             <Link to="/"> Əsas səhifə</Link>
             <Link to="/services"> Haqqımızda</Link>
             <Link to="/about"> Məhsullar</Link>
@@ -45,24 +45,24 @@ const Footer = () => {
             <Link to="/contact"> Əlaqə</Link>
           </div>
         </div>
-        <div className="right flex flex-col">
-          <h2 className=" text-2xl font-bold text-[white] ">Əlaqə</h2>
-          <div className="flex  gap-10 text-[#ffffff81] py-4">
-          <div className="flex flex-col  " >
-            <div className="flex flex-col gap-2">
+        <div className=" flex ">
+          <div className="flex  gap-4 text-[#ffffff81] ">
+          <div className="flex flex-col" >
+          <h2 className=" text-[24px] font-bold text-[white] ">Əlaqə</h2>
+            <div className="flex flex-col ">
               <h2 className="text-bold text-[18px] text-[white]">Telefon</h2>
               <div >
                 <p>+99450 5754080</p>
                 <p>+99450 5754080</p>
               </div>
             </div>
-            <div className="flex flex-col py-6 gap-2">
+            <div className="flex flex-col py-3 gap-2">
               <h2  className="text-bold text-[18px] text-[white]">Email</h2>
               <p>info@netprogroup.az </p>
             </div>
           </div>
           <div className="flex flex-col gap-2 ">
-            <h2  className="text-bold text-[18px] text-[white]">Ünvan</h2>
+            <h2 className="text-bold text-[18px] text-[white]">Ünvan</h2>
             <p>Fəzail Bayramov küç., 1156, Xətai ray., Bakı, Azərbaycan, AZ1025</p>
             <img src="./map.png" alt="" />
           </div>
