be958080e1c3b66003d45d2cab3f206b90b044ec
QulamSamadli <qulam999@gmail.com>
2024-01-21 01:43:46 +0400

    added new codes

--- src/Pages/Services/index.jsx
+++ src/Pages/Services/index.jsx
@@ -11,25 +11,32 @@ const Services = () => {
       setSolution(data);
     });
   }, []);
-
   return (
-    <div className="container ">
+    <div className=" ">
       <HeaderBottom title1="salam" title2="salam2" title3="salam1" />
 
-      <div className="flex flex-wrap justify-center">
-      {solution.map(({ id, img, title, description }) => {
-        return (
-          <div className="border-[#D4DAEA] w-[290px] p-4 flex flex-col gap-2 border-[1px] rounded-[10px]">
-            <img src={img} alt="" />
-            <h2 className="text-[18px] h-14 text-[#435072] font-bold">{title}</h2>
-            <hr />
-            
-            <p className="text-[13px]  text-[#606F84] font-normal">
-              {description}
-            </p>
-          </div>
-        );
-      })}
+      <div className="bg-[#F4F4F4] p-6 container flex flex-col">
+        <h2 className=" text-center  text-[32px] text-[#435072] font-bold ">
+          Xidmətlər və Həllərin növləri
+        </h2>
+
+        <div className="flex flex-wrap justify-center gap-5  ">
+          {solution.map(({ id, img, title, description }) => {
+            return (
+              <div className="border-[#D4DAEA] w-[290px] p-4 flex flex-col gap-2 border-[1px] rounded-[10px]">
+                <img src={img} alt="" />
+                <h2 className="text-[18px] h-14 text-[#435072] font-bold">
+                  {title}
+                </h2>
+                <hr />
+
+                <p className="text-[13px]  text-[#606F84] font-normal">
+                  {description}
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
