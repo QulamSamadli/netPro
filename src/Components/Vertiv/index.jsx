8c5c5fae41fcc1129d5cfc113855fefcad0b1d6c
QulamSamadli <qulam999@gmail.com>
2024-01-22 16:23:18 +0400

    added codes

--- src/Components/Vertiv/index.jsx
+++ src/Components/Vertiv/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeaderBottom from '../HeaderBottom'
+import Category from '../common/Category'
 
 const Vertiv = () => {
   return (
-    <div>
-<HeaderBottom title1={"VERTIV"} title2={"Əsas səhifə"} title3={"Vertiv"} title4={"-->Fasiləsiz Enerji Təchizatı"}/>
+    <div >
+<HeaderBottom  title1={"VERTIV"} title2={"Əsas səhifə"} title3={"Vertiv"} title4={"-->Fasiləsiz Enerji Təchizatı"}/>
     
+    <Category/>
     </div>
   )
 }
---
+++ src/Components/common/Category/index.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+const _url = "http://localhost:5382/category1";
+
+const Category = () => {
+  const [category, setCategory] = useState([]);
+  useEffect(() => {
+    axios.get(_url).then(({ data }) => {
+      setCategory(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className="w-[356px] border-solid border-[#D4DAEA] rounded-lg border-2">
+        <h2 className="text-[24px] font-bold p-4 border-solid border-[##D4DAEA] bg-[#435072] text-white ">
+          Category
+        </h2>
+        <div>
+          {category.map(({ title }) => {
+            return (
+              <Link
+                to="/"
+                className="text-[#435072] p-4   text-[18px] font-bold"
+              >
+                {title} <hr />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
