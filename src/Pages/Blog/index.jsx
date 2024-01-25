280ac17c0a54edd35c38fc0877c4717f0c741c75
QulamSamadli <qulam999@gmail.com>
2024-01-26 01:43:20 +0400

    finished Blog pages

--- src/Pages/Blog/index.jsx
+++ src/Pages/Blog/index.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import HeaderBottom from '../../Components/HeaderBottom'
+
+const _url = "http://localhost:5722/blog"
 
 const Blog = () => {
+  const [blog,setBlog] = useState([])
+
+  useEffect(()=>{
+axios.get(_url).then(({data})=>{
+  setBlog(data)
+})
+
+  },[])
   return (
-    <div>Blog</div>
+
+    <div>
+<HeaderBottom title1={"Qalereya və Bloq"} title2={"Əsas səhifə"} title3={"Qalereya və Bloq"}/>
+  <h2 className='text-center my-4 text-[#435072] text-[40px] font-bold'>Qalereya</h2>
+<div className='flex flex-wrap gap-4 justify-center my-6'>
+      {
+        blog.map(({id,img})=>{
+          return <div >
+             <img key={id} src={img} alt={""} />
+          </div>
+        })
+      }
+    </div>
+    </div>
   )
 }
 
