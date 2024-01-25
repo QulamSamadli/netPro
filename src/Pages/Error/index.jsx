bc504ff704c58d7401753a9374d04f97dccf7f67
QulamSamadli <qulam999@gmail.com>
2024-01-26 01:48:25 +0400

    Eror pages finished

--- src/Pages/Error/index.jsx
+++ src/Pages/Error/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Button from '../../Components/common/Button'
 
 const Error = () => {
   return (
-    <div>404 Not found</div>
+    <div className='flex justify-center items-center gap-6'>
+
+<div className='flex flex-col gap-6'>
+  <img src="./not404.png" alt="" />
+  <p>Axtardığınız Səhifə 
+Tapılmadı!</p>
+ <Button title={"Əsas səhifəyə qayıt"} to="/"/>
+</div>
+<div><img src="./build.png" alt="" /></div>
+
+    </div>
   )
 }
 
